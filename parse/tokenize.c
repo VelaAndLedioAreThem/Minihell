2df2cbf5f38dff44bc8fa165f4e707dd6037553e
Miami05 <lediodurmishaj16@gmail.com>
2025-02-13 17:32:15 +0100

    A

--- parse/tokenize.c
+++ parse/tokenize.c
@@ -6,7 +6,7 @@
 /*   By: ldurmish < ldurmish@student.42wolfsburg.d  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 15:33:10 by ldurmish          #+#    #+#             */
-/*   Updated: 2025/02/13 16:44:01 by ldurmish         ###   ########.fr       */
+/*   Updated: 2025/02/13 17:30:30 by ldurmish         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ t_token	*tokenize(char *input)
 		return (NULL);
 	while (input[i])
 	{
-		if ((input[i]))
+		if (ft_isspace(input[i]))
 			i++;
 	}
 }
