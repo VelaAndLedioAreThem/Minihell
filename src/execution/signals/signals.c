c94747c79bf7a49e1f0d769e5aa643289e2845da
Velaguez Szpiech <vszpiech@c1r16s3.42wolfsburg.de>
2025-06-28 18:03:10 +0200

    hello

--- src/execution/signals/signals.c
+++ src/execution/signals/signals.c
@@ -5,50 +5,48 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: vszpiech <vszpiech@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/25 13:17:20 by vszpiech          #+#    #+#             */
-/*   Updated: 2025/06/28 12:04:55 by vszpiech         ###   ########.fr       */
+/*   Created: 2025/06/28 18:02:16 by vszpiech          #+#    #+#             */
+/*   Updated: 2025/06/28 18:02:17 by vszpiech         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../include/minishell.h"
 
-static void	signal_child(void)
+static void	sigint_handler(int sig)
 {
-	kill(g_ctx->child_pid, SIGINT);
-	g_ctx->child_pid++;
-}
-
-void	handle_sigint(int signo)
-{
-	int	i;
-
-	if (signo != SIGINT)
-		return ;
-	if (!isatty(STDIN_FILENO))
-		exit(EXIT_SUCCESS);
-	i = write(1, "\n", 1);
-	(void)i;
-	if (g_ctx->child_pid == 42)
-		g_ctx->child_pid = 44;
-	if (g_ctx->child_pid != 0 && g_ctx->child_pid != 44)
-		signal_child();
-	else
-		redisplay_prompt();
-}
-
-void	handle_sigtstp_sigquit(int signo)
-{
-	if (signo == SIGTSTP || signo == SIGQUIT)
+	(void)sig;
+	if (g_ctx->child_pid == 0)
 	{
-		if (isatty(STDIN_FILENO))
-		{
-			if (g_ctx->child_pid == 0)
-				rl_redisplay();
-			else
-			{
-				kill(g_ctx->child_pid, signo);
-				g_ctx->child_pid += 2;
-			}
-		}
+		ft_putstr_fd("\n", STDOUT_FILENO);
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
 	}
+	else
+	{
+		ft_putstr_fd("\n", STDOUT_FILENO);
+		kill(g_ctx->child_pid, SIGINT);
+	}
+}
+
+void	handle_signal(t_ctx *ctx)
+{
+	struct sigaction	sa_int;
+	struct sigaction	sa_quit;
+
+	g_ctx = ctx;
+	sa_int.sa_handler = sigint_handler;
+	sigemptyset(&sa_int.sa_mask);
+	sa_int.sa_flags = SA_RESTART;
+	sigaction(SIGINT, &sa_int, NULL);
+	sa_quit.sa_handler = SIG_IGN;
+	sigemptyset(&sa_quit.sa_mask);
+	sa_quit.sa_flags = 0;
+	sigaction(SIGQUIT, &sa_quit, NULL);
+}
+
+void	redisplay_prompt(void)
+{
+	rl_on_new_line();
+	rl_redisplay();
 }
